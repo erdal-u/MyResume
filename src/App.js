f356c9c17caf81d4959f73b940099f842aabeb6e
erdal-u <48831628+erdal-u@users.noreply.github.com>
2020-03-15 13:26:01 +0100

    resume add working progress

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
+import ErdalCv from './assets/ErdalCv.jpg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={ErdalCv} className="App-logo" alt="ErdalCv" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
           <h1> detta är ett test för att se att det funkar</h1>
