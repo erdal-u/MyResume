bee5299f39128c3d12b04624c1c2544ebc5d5732
erdal-u <48831628+erdal-u@users.noreply.github.com>
2020-04-01 11:26:26 +0200

    försöker pusha till ghpages

--- src/index.js
+++ src/index.js
@@ -4,10 +4,6 @@ import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
-const { addDecoratorsLegacy, useEslintRc, override } = require("customize-cra");
-
-module.exports = override(addDecoratorsLegacy(), useEslintRc("./.eslintrc"));
-
 ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
